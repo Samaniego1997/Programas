61d6b006df18715db6a2df1efb1e078f314f67ac
Samaniego1997 <144870184+Samaniego1997@users.noreply.github.com>
2023-09-15 07:24:08 -0500

    Add files via upload

---
+++ SumaSamaniegoDenis.cpp
@@ -0,0 +1,19 @@
+/*programa:suma.cpp
+  autor:SamaniegoDenis
+fecha:2023-09-14
+descripcion:suma dos numeros
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+	float x,y,z;
+	cout<<"Ingrese x:";cin>>x;
+	cout<<"Ingrese y:";cin>>y;
+	//Aqui se realiza la suma
+	z=x+y;
+	cout<<"la suma de"<<x<<"+"<<y<<"="<<z<<endl;
+	return(0);
+}
+
+
